1a6c763b9f77ac5c24422bf189a8d6ac856ea477
murilo-dan <murilo.loiola.dan@gmail.com>
2020-11-28 20:14:16 -0300

    soap basic function

--- soap/index.js
+++ soap/index.js
@@ -11,22 +11,18 @@ var myService = {
                 };
             },
   
-            // This is how to define an asynchronous function.
             MyAsyncFunction: function(args, callback) {
-                // do some work
                 callback({
                     name: args.name
                 });
             },
   
-            // This is how to receive incoming headers
             HeadersAwareFunction: function(args, cb, headers) {
                 return {
                     name: headers.Token
                 };
             },
   
-            // You can also inspect the original `req`
             reallyDetailedFunction: function(args, cb, headers, req) {
                 console.log('SOAP `reallyDetailedFunction` request from ' + req.connection.remoteAddress);
                 return {
@@ -46,4 +42,3 @@ app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
 app.listen(3000, function(){
     soap.listen(app, '/wsdl', myService ,xml);
 });
-
