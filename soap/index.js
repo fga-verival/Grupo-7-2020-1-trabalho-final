61e224bc4e4a0a27ed3b872e98f37315fe1460fb
Ed-vL <eduardolima.df@gmail.com>
2020-11-28 20:45:06 -0300

    Adding required functions
    Co-authored-by: Murilo Loiola <murilo.loiola.dan@gmail.com>

--- soap/index.js
+++ soap/index.js
@@ -2,33 +2,60 @@ const express = require('express');
 const soap = require('soap');
 const bodyParser = require('body-parser')
 
+const mock = {
+	nome: "João",
+	conta: 111,
+	saldo: 123,
+	transacoes: [
+		{
+		valor: 123,
+		contaDestinatario: 123,
+		data: "1/1/2001" 
+		},
+		
+		{
+		valor: 132,
+		contaDestinatario: 132,
+		data: "1/1/2003" 
+		},
+		
+		{
+		valor: 321,
+		contaDestinatario: 321,
+		data: "1/1/2004" 
+		}
+	],
+	contatos: [
+		{
+		nome: "Maria",
+		conta: 123
+		},
+		
+		{
+		nome: "Pedro",
+		conta: 132
+		}
+	],
+	limite: 400,
+	limiteDisponivel: 135,
+	valorElegivelParaEmprestimo: 10000,
+	pontos: 0
+}
+
 var myService = {
     MyService: {
         MyPort: {
-            MyFunction: function(args) {
-                return {
-                    name: args.name
-                };
+            getOne: function (args) {
+                return {number: 1};
             },
-  
-            MyAsyncFunction: function(args, callback) {
-                callback({
-                    name: args.name
-                });
+
+            login: function (args) {
+                return {user_session: "123456"};
             },
-  
-            HeadersAwareFunction: function(args, cb, headers) {
-                return {
-                    name: headers.Token
-                };
+
+            getUser: function (args) {
+                return mock
             },
-  
-            reallyDetailedFunction: function(args, cb, headers, req) {
-                console.log('SOAP `reallyDetailedFunction` request from ' + req.connection.remoteAddress);
-                return {
-                    name: headers.Token
-                };
-            }
         }
     }
 };
@@ -37,8 +64,8 @@ var app = express();
 
 var xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
 
-app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
+app.use(bodyParser.raw({ type: function () { return true; }, limit: '5mb' }));
 
-app.listen(3000, function(){
-    soap.listen(app, '/wsdl', myService ,xml);
+app.listen(3000, function () {
+    soap.listen(app, '/wsdl', myService, xml);
 });
