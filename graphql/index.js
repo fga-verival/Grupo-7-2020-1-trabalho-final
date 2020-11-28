ba7bcb066b0e57c09e13d5537b2fade9d71c86d5
Rafael Teodosio <rafzteodosio@gmail.com>
2020-11-28 16:59:52 -0300

    Finalizando teste 2 API GraphQL
    Co-authored-by: Luis Taira <luishptaira@gmail.com>

--- graphql/index.js
+++ graphql/index.js
@@ -1,25 +1,122 @@
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
- 
+var express = require("express");
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema } = require("graphql");
 var schema = buildSchema(`
-  type Query {
+  
+type User{
+        nome: String
+        conta: Int
+        saldo: Int
+}
+
+type Transacao{
+    valor: Int
+    contaDestinatario: Int
+    data: String
+}
+
+type Contato{
+    nome: String
+    conta: Int
+}
+
+type Limite{
+    limite: Int
+    limiteDisponivel: Int
+}
+
+type Emprestimo{
+    valorElegivelParaEmprestimo: Int
+}
+
+type Pontos{
+    pontos: Int 
+}
+
+type Query {
     test1: Int,
-  }
+    login (email: String!, senha: String!): String,
+    getUser(auth: String!): User,
+    getTransacao(auth: String!): [Transacao],
+    getContato(auth: String!): [Contato],
+    getLimite(auth: String!): Limite,
+    getEmprestimo(auth: String!): Emprestimo,
+    getPontos(auth: String!): Pontos,
+    }
 `);
- 
+
 var root = {
-  test1: () =>{
-      return 1;
+  test1: () => {
+    return 1;
+  },
+  login: (email, senha) => {
+    return "123213";
+  },
+  getUser: (auth) => {
+    return {
+      nome: "João",
+      conta: 111,
+      saldo: 123,
+    };
+  },
+  getTransacao: (auth) => {
+    return [
+      {
+        valor: 123,
+        contaDestinatario: 123,
+        data: "1/1/2001",
+      },
+      {
+        valor: 132,
+        contaDestinatario: 132,
+        data: "1/1/2003",
+      },
+      {
+        valor: 321,
+        contaDestinatario: 321,
+        data: "1/1/2004",
+      },
+    ];
+  },
+  getContato: (auth) => {
+    return [
+      {
+        nome: "Maria",
+        conta: 123,
+      },
+      {
+        nome: "Pedro",
+        conta: 132,
+      },
+    ];
+  },
+  getLimite: (auth) => {
+    return {
+      limite: 400,
+      limiteDisponivel: 135
+    };
+  },
+  getEmprestimo: (auth) => {
+    return {
+        valorElegivelParaEmprestimo: 10000
+    };
+  },
+  getPontos: (auth) => {
+    return {
+      pontos: 0
+    };
   },
 };
- 
+
 var app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(3001);
