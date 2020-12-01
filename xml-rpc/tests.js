2d2007f67bafc5b0b4f25651d5e78755f8347b8a
Rafael Ribeiro <rafaelflarrn@gmail.com>
2020-12-01 19:03:31 -0300

    Fix test script and client configuration
    Co-authored-by: Samuel Buters <sambuters@gmail.com>
    Co-authored-by: Luis Taira <luishptaira@gmail.com>

--- xml-rpc/tests.js
+++ xml-rpc/tests.js
@@ -5,21 +5,15 @@ const chai = require('chai');
 const should = chai.should();
 
 describe('xmlrpc', () => {
+    var client
     before(() => {
-        return new Promise((resolve) => {
-            var clientOptions = {
-                host: 'localhost',
-                port: 3005,
-                path: '/'
-            }
-            var client = xmlrpc.createClient(clientOptions)
-            
-        setTimeout(() => {
-            
-            resolve() 
-            }, 200)
-        })
-    });
+        var clientOptions = {
+            host: 'localhost',
+            port: 3005,
+            path: '/'
+        }
+        client = xmlrpc.createClient(clientOptions)
+    })
 
     after(() => {
         process.exit(0);
@@ -27,26 +21,43 @@ describe('xmlrpc', () => {
             
 
     it('should return 1', (done) => {
-        console.log('GetOne')
         client.methodCall('getOne', null, function(err, value) {
             
             if(err)
                 console.log(err);
             else {
-                value.should.be.an('Object');
-                value.should.have.property('number').that.is.a('Number').equal(1);
-                done();
+                value.should.be.a('Number').eql(1);
+            done();
             }
         });
     })
-    
-    it('should return user info', (done) => {
-        client.methodCall('getNome', null, function(err, value) {
+
+    it('shold return user session', (done) => {
+        client.methodCall('getUserSession', null, function(err, value) {
             if(err)
-                console.log(err);
+                console.log(err)
             else {
-                value.should.be.an('Object');
-                value.should.have.property('nome').that.is.a('string').eql('João');
+                value.should.be.a('Number').eql(123)
+                done();
+            }
+        })
+    })
+
+    it('should return user info', (done) => {
+        client.methodCall('getUserInfo', null, function(err, value) {
+            if(err)
+				console.log(err);
+			else {
+				value.should.be.an('Object');
+				value.should.have.property('nome').that.is.a('string').equal('João');
+				value.should.have.property('conta').that.is.a('Number').equal(111);
+				value.should.have.property('saldo').that.is.a('Number').equal(123);
+				value.should.have.property('transacoes').that.is.an('Array').that.has.lengthOf(3);
+				value.should.have.property('contatos').that.is.an('Array').that.has.lengthOf(2);
+				value.should.have.property('limite').that.is.an('Number').equal(400);
+				value.should.have.property('limiteDisponivel').that.is.an('Number').equal(135);
+				value.should.have.property('valorElegivelParaEmprestimo').that.is.an('Number').equal(10000);
+				value.should.have.property('pontos').that.is.an('Number').equal(0);
                 done();
             }
         })
